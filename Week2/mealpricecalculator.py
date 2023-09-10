a37cf4eb5acd5a270ccc61eb28c5edef4ce93fdc
Humpherys-Ross <ross.humpherys@gmail.com>
2023-09-10 17:27:56 -0400

    finish final mealpricecalculator

--- Week2/mealpricecalculator.py
+++ Week2/mealpricecalculator.py
@@ -1,5 +1,8 @@
 # Ask the user for the following:
 
+# Added the ability to enter a tip percentage and calculate the tip amount and total price of the meal.
+# Added the ability to enter beverages and calculate the total price of the meal.
+
 # - The price of a child's meal (floating point)
 # - The price of an adult's meal (floating point)
 # - The number of children (integer)
@@ -45,3 +48,49 @@ subtotal = (child_meal * num_children) + (adult_meal * num_adults)
 print(f'The subtotal is ${subtotal:.2f}')
 
 # ---------------------------------------------------------------------------------------------
+
+# Final requirements for Meal Price Calculator plus stretch challenges
+
+# Ask if the user wants to add beverages to the meal. If they do, ask for the number of beverages and the price per beverage.
+beverages = input('Would you like to add beverages to the meal? (y/n) ')
+
+if beverages == 'y':
+    num_beverages = int(input('How many beverages would you like to add? '))
+    price_beverage = float(input('What is the price per beverage? '))
+    subtotal += num_beverages * price_beverage
+
+print(f'The subtotal is ${subtotal:.2f}')
+
+# Ask for sales tax rate as a percentage (floating point)
+sales_tax_rate = float(input('What is the sales tax rate? '))
+
+# Compute and display the sales tax, by multiplying the subtotal by the sales tax rate divided by 100.
+sales_tax = subtotal * (sales_tax_rate / 100)
+
+# display the sales tax
+print(f'The sales tax is ${sales_tax:.2f}')
+
+# Ask the user for the tip percentage (floating point) and calculate the tip amount and total price of the meal.
+tip = input('Would you like to add a tip? (y/n) ')
+
+if tip == 'y':
+    tip_percentage = float(input('What is the tip percentage? '))
+    tip_amount = subtotal * (tip_percentage / 100)
+    subtotal += tip_amount
+
+print(f'The subtotal is ${subtotal:.2f}')
+
+# Compute and display the total price of the meal by adding the subtotal and the sales tax.
+total_price = subtotal + sales_tax
+
+# display the total price
+print(f'The total price is ${total_price:.2f}')
+
+# Ask the user for the the payment amount (floating point)
+payment_amount = float(input('What is the payment amount? '))
+
+# Compute and display the change.
+change = payment_amount - total_price
+
+# display the change
+print(f'The change is ${change:.2f}')
