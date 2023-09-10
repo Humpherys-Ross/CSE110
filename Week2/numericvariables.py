16f6633f6cf09553ff9994a8da2acd5e204d1356
Humpherys-Ross <ross.humpherys@gmail.com>
2023-09-10 10:50:51 -0400

    Completed numeric variables

---
+++ Week2/numericvariables.py
@@ -0,0 +1,10 @@
+# age = input('What is your age? ')
+# print(f'On your next birthday, you will be {int(age) + 1}')
+
+# eggs = input('How many egg cartons do you have? ')
+# print(f'You have {int(eggs) * 12} eggs')
+
+# cookies = int(input('How many cookies do you have? '))
+# people = int(input('How many people are there? '))
+# cookies_per_person = cookies / people
+# print(f'Each person may have {cookies_per_person} cookies')
