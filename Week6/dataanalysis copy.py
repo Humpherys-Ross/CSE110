7b53062ce8d7106ded9799f3aed42c85109ab2a1
Humpherys-Ross <ross.humpherys@gmail.com>
2023-10-20 17:58:58 -0400

    rename files

--- Week6/dataanalysis copy.py
+++ Week6/dataanalysis copy.py
@@ -1,6 +1,4 @@
 # Allow for the user to search via year or country
-# I also refactored the code to use functions instead of just one big block of
-# code. This makes it easier to read and understand what is going on.
 
 # Download the dataset and write a Python program to analyze it to answer the
 # following questions:
@@ -45,217 +43,135 @@
 
 # #####################################
 
+# initialize variables to store max and min life expectancy
+# use infinity as the initial value
+min_life_expectancy = float("inf")
+min_life_country = ""
+min_life_year = ""
 
-# function to load the data from the provided file
-def load_data(filename):
-    # create a dictionary to store the average life expectancy for each year
-    life_expectancies_by_year = {}
-    life_expectancies_by_country = {}
+# use negative infinity as the initial value
+max_life_expectancy = float("-inf")
+max_life_country = ""
+max_life_year = ""
 
-    # initialize variables to store max and min life expectancy
-    min_life_expectancy = float('inf')
-    max_life_expectancy = float('-inf')
-    min_life_country = ''
-    max_life_country = ''
-    min_life_year = 0
-    max_life_year = 0
+# create a dictionary to store the average life expectancy for each year
+life_expectancies_by_year = {}
 
-    # open the file
-    with open(filename, 'r') as file:
-        # skip the first line
-        next(file)
+# create a dictionary to store the average life expectancy for each country
+life_expectancies_by_country = {}
 
-        # iterate through the data line by line
-        for line in file:
-            # strip the newline character and split the line into parts
-            parts = line.strip().split(',')
-            # unpack the parts into variables
-            entity, code, year, life_expectancy = parts
-            # convert year and life_expectancy to the appropriate data types
-            year = int(year)
-            life_expectancy = float(life_expectancy)
+# open the file, week6/life-expectancy.csv and read it line by line
+with open("Week6/life-expectancy.csv", "r") as file:
+    # skip the first line
+    next(file)
 
-            # find the lowest value for life expectancy and the highest value
-            # for life expectancy in the dataset
-            if life_expectancy < min_life_expectancy:
-                min_life_expectancy = life_expectancy
-                min_life_country = entity
-                min_life_year = year
+    for line in file:
+        # split the line into parts stripping the newline character
+        # and splitting on the comma
+        parts = line.strip().split(",")
 
-            if life_expectancy > max_life_expectancy:
-                max_life_expectancy = life_expectancy
-                max_life_country = entity
-                max_life_year = year
+        # get the year, country, and life expectancy from the parts
+        entity, code, year, life_expectancy = parts
+        year = int(year)
+        life_expectancy = float(life_expectancy)
 
-            # store the average life expectancy for each year
-            if year not in life_expectancies_by_year:
-                life_expectancies_by_year[year] = []
+        # check if the current life expectancy is the new min
+        if life_expectancy < min_life_expectancy:
+            min_life_expectancy = life_expectancy
+            min_life_country = entity
+            min_life_year = year
 
-            life_expectancies_by_year[year].append((entity, life_expectancy))
+        # check if the current life expectancy is the new max
+        if life_expectancy > max_life_expectancy:
+            max_life_expectancy = life_expectancy
+            max_life_country = entity
+            max_life_year = year
 
-            # store the average life expectancy for each country
-            if entity not in life_expectancies_by_country:
-                life_expectancies_by_country[entity] = []
+        # check if the year is already in the dictionary
+        if year not in life_expectancies_by_year:
+            life_expectancies_by_year[year] = []
 
-            life_expectancies_by_country[entity].append((year,
-                                                         life_expectancy))
+        # add the life expectancy to the list for the year
+        life_expectancies_by_year[year].append((entity, life_expectancy))
 
-    # return the life_expectancies_by_year and life_expectancies_by_country
-    # dictionaries and the min and max life expectancies and their
-    # corresponding countries and years
-    return (
-        life_expectancies_by_year,
-        life_expectancies_by_country,
-        min_life_expectancy,
-        min_life_country,
-        min_life_year,
-        max_life_expectancy,
-        max_life_country,
-        max_life_year
-    )
+        # check if the country is already in the dictionary
+        if entity not in life_expectancies_by_country:
+            life_expectancies_by_country[entity] = []
 
+        # add the life expectancy to the list for the country
+        life_expectancies_by_country[entity].append((year, life_expectancy))
 
-# function to get the data for a year
-def get_year_data(year, data_by_year):
-    # check if the year is in the dictionary
-    if year not in data_by_year:
-        # if not, return None
-        return None
+# ask the user whether they want to search by year or country
+search_type = input("Do you want to search by (Y)ear or (C)ountry? ")
+search_type = search_type.strip().lower()  # This is for formatting purposes
 
-    # get the life expectancies for the year
-    life_expectancies = [le for _, le in data_by_year[year]]
-    avg_life_expectancy = sum(life_expectancies) / len(life_expectancies)
+if search_type == "y":
 
-    # set the min and max life expectancies variables
+    # ask the user for a year
+    user_year = int(input("Enter the year of interest: "))
+
+    # calculate the average life expectancy for the year
+    if user_year in life_expectancies_by_year:
+        life_expectancies = [le for _, le in
+                             life_expectancies_by_year[user_year]]
+        avg_life_expectancy = sum(life_expectancies) / len(life_expectancies)
+    else:
+        avg_life_expectancy = None
+
+    # find the max and min life expectancies for the year
     min_life_country_user = ""
     min_life_expectancy_user = float("inf")
 
     max_life_country_user = ""
     max_life_expectancy_user = float("-inf")
 
-    for entity, life_expectancy in data_by_year[year]:
-        if life_expectancy < min_life_expectancy_user:
-            min_life_expectancy_user = life_expectancy
-            min_life_country_user = entity
+    if user_year in life_expectancies_by_year:
+        for entity, life_expectancy in life_expectancies_by_year[user_year]:
+            if life_expectancy < min_life_expectancy_user:
+                min_life_expectancy_user = life_expectancy
+                min_life_country_user = entity
 
-        if life_expectancy > max_life_expectancy_user:
-            max_life_expectancy_user = life_expectancy
-            max_life_country_user = entity
+            if life_expectancy > max_life_expectancy_user:
+                max_life_expectancy_user = life_expectancy
+                max_life_country_user = entity
 
-    # return the average life expectancy, the country with the min life
-    # expectancy, and the country with the max life expectancy for the year
-    return {
-        "avg_life_expectancy": avg_life_expectancy,
-        "min_life_expectancy_country": min_life_country_user,
-        "min_life_expectancy": min_life_expectancy_user,
-        "max_life_expectancy_country": max_life_country_user,
-        "max_life_expectancy": max_life_expectancy_user
-    }
+    # print the results
+    print(f"The overall max life expectancy is: {max_life_expectancy} from"
+          f" {max_life_country} in {max_life_year}")
+    print(f"The overall min life expectancy is: {min_life_expectancy} from"
+          f" {min_life_country} in {min_life_year}")
 
+    # print the results for the user's year
+    if avg_life_expectancy is not None:
+        print(f"\nFor the year {user_year}:")
+        print(f"The average life expectancy across all countries was"
+              f" {avg_life_expectancy:.2f}")
+        print(f"The max life expectancy was in {max_life_country_user} with"
+              f" {max_life_expectancy_user}")
+        print(f"The min life expectancy was in {min_life_country_user} with"
+              f" {min_life_expectancy_user}")
+    else:
+        print(f"\nNo data for {user_year}")
+elif search_type == 'c':
+    # ask the user for a country
+    user_country = input("Enter the country of interest: ").capitalize()
 
-# function to get the data for a country
-def get_country_data(country, data_by_country):
-    # check if the country is in the dictionary
-    if country not in data_by_country:
-        # if not, return None
-        return None
+    if user_country in life_expectancies_by_country:
+        # find the data for the country
+        country_data = life_expectancies_by_country[user_country]
 
-    # get the life expectancies for the country
-    country_data = data_by_country[country]
+        # find the max and min life expectancies for the country
+        # use the max and min functions with a lambda function
+        max_year_country = max(country_data, key=lambda x: x[1])
+        min_year_country = min(country_data, key=lambda x: x[1])
 
-    # find the year with the highest and lowest life expectancy for the country
-    # using a lambda function
-    max_year_country = max(country_data, key=lambda x: x[1])
-    min_year_country = min(country_data, key=lambda x: x[1])
-
-    # return the year with the highest and lowest life expectancy for the
-    # country
-    return {
-        "highest_life_expectancy_year": max_year_country[0],
-        "highest_life_expectancy": max_year_country[1],
-        "lowest_life_expectancy_year": min_year_country[0],
-        "lowest_life_expectancy": min_year_country[1]
-    }
-
-
-# main function
-def main():
-    # define the filename path and load the data
-    filename = 'Week6/life-expectancy.csv'
-    (
-        life_expectancies_by_year,
-        life_expectancies_by_country,
-        min_life_expectancy,
-        min_life_country,
-        min_life_year,
-        max_life_expectancy,
-        max_life_country,
-        max_life_year
-    ) = load_data(filename)
-
-    # use a while true loop to allow the user to search multiple times
-    while True:
-        # ask the user if they want to search by year or country
-        user_choice = input("Do you want to search by (Y)ear or (C)ountry? ")
-        # strip and convert the user's choice to lowercase
-        user_choice = user_choice.strip().lower()
-
-        if user_choice == "y":
-            while True:
-                try:
-                    user_year = int(input("Enter the year of interest: "))
-                    year_data = get_year_data(user_year,
-                                              life_expectancies_by_year)
-                    if year_data:
-                        break
-                    else:
-                        print(f"No data available for {user_year}")
-                except ValueError:
-                    print("Invalid year. Please enter a valid year.")
-
-            print(f"The overall max life expectancy is:"
-                  f" {max_life_expectancy:.3f} from {max_life_country} in"
-                  f" {max_life_year}")
-            print(f"The overall min life expectancy is:"
-                  f" {min_life_expectancy:.3f} from {min_life_country} in"
-                  f" {min_life_year}")
-
-            print(f"\nFor the year {user_year}:")
-            print(f"The average life expectancy across all countries was:"
-                  f" {year_data['avg_life_expectancy']:.3f}")
-            print(f"The max life expectancy was in"
-                  f" {year_data['max_life_expectancy_country']} with"
-                  f" {year_data['max_life_expectancy']:.3f}")
-            print(f"The min life expectancy was in"
-                  f" {year_data['min_life_expectancy_country']} with"
-                  f" {year_data['min_life_expectancy']:.3f}")
-        elif user_choice == "c":
-            while True:
-                user_country = input("Enter the country of interest: ")
-                user_country = user_country.strip().capitalize()
-                country_data = get_country_data(user_country,
-                                                life_expectancies_by_country)
-                if country_data:
-                    break
-                else:
-                    print(f"No data available for the country {user_country}.")
-
-            print(f"Country: {user_country}")
-            print(f"The highest life expectancy was in"
-                  f" {country_data['highest_life_expectancy_year']} in"
-                  f" {country_data['highest_life_expectancy']:.3f} years.")
-            print(f"The lowest life expectancy was in"
-                  f" {country_data['lowest_life_expectancy_year']} in"
-                  f" {country_data['lowest_life_expectancy']:.3f} years.")
-        else:
-            print("Invalid choice. Please enter 'Y' for year or 'C' for"
-                  " country.")
-
-        search_again = input("Do you want to search again? (Y/N): ")
-        search_again = search_again.strip().lower()
-        if search_again != "y":
-            break
-
-
-if __name__ == "__main__":
-    main()
+        # display the results for the user-specified country
+        print(f"Country: {user_country}")
+        print(f"The highest life expectancy was in {max_year_country[0]} in"
+              f" {max_year_country[1]:.2f} years.")
+        print(f"The lowest life expectancy was in {min_year_country[0]} in"
+              f" {min_year_country[1]:.2f} years.")
+    else:
+        print(f"\nNo data for {user_country}")
+else:
+    print("Invalid choice. Please type "'Y'" for year or "'C'" for country.")
